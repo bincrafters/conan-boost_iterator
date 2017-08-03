d4057b38bda01c30101b810d7f07d2c40481fc10
Jerry W <jerrywiltse@gmail.com>
2017-08-03 11:51:51 -0400

    switched to level5group requirement

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools, os
+from conans import ConanFile
 
 class BoostIteratorConan(ConanFile):
     name = "Boost.Iterator"
@@ -7,31 +7,15 @@ class BoostIteratorConan(ConanFile):
     url = "https://github.com/boostorg/iterator"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "iterator"
-    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Conversion/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Detail/1.64.0@bincrafters/testing", \
-                      "Boost.Function_Types/1.64.0@bincrafters/testing", \
-                      "Boost.Fusion/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Optional/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
-                      "Boost.Utility/1.64.0@bincrafters/testing"
-    
-                      #assert1 concept_check5 config0 conversion5 core2 detail5 function_types5 fusion5 mpl5 optional5 smart_ptr4 static_assert1 type_traits3 utility5
-    
-    def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
-
-    def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
-
+    requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
+   
     def package_id(self):
         self.info.header_only()
+        
+    #This library is part of one or more cyclic dependency groups within Boost.
+    
+    #All members of cyclic dependency groups must be built under single package per group for Conan.
+    
+    #The combination is performed in the package(s) listed in the requires field.
+    
+    #This package enables simple consumption of this library while abstracting away the cyclic dependency issues. 
