971e2e32f8fada28affa97893cceb3699648cbf9
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:09 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostIteratorConan(ConanFile):
     name = "boost_iterator"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_iterator"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["iterator"]
     is_in_cycle_group = True
     is_header_only = True
