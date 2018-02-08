f0b3ed72391b0d3674a1af831cd7da8ba5ddc3a4
Jerry W <jerrywiltse@gmail.com>
2018-02-07 20:01:51 -0500

    add level_group property to group members

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class BoostIteratorConan(ConanFile):
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     lib_short_names = ["iterator"]
-    is_in_cycle_group = True
+    level_group = "boost_level5group"
     is_header_only = True
 
     def package_id_additional(self):
