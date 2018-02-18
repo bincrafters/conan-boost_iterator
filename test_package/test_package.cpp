53427c67010355ff74581331258cc37f907ef7eb
SSE4 <tomskside@gmail.com>
2018-02-18 21:16:59 +0700

    - add test_package

---
+++ test_package/test_package.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iterator>
+#include <boost/iterator/filter_iterator.hpp>
+
+struct is_odd
+{
+    bool operator()(int a) { return 0 != a % 2; }
+};
+
+struct is_even
+{
+    bool operator()(int a) { return 0 == a % 2; }
+};
+
+int main()
+{
+    std::vector<int> v(10);
+    std::iota(v.begin(), v.end(), 0);
+    std::cout << "odd numbers: ";
+    std::copy(
+        boost::make_filter_iterator<is_odd>(v.begin(), v.end()),
+        boost::make_filter_iterator<is_odd>(v.end(), v.end()),
+        std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "even numbers: ";
+    std::copy(
+        boost::make_filter_iterator<is_even>(v.begin(), v.end()),
+        boost::make_filter_iterator<is_even>(v.end(), v.end()),
+        std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
