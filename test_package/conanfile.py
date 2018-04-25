81f6ac95695078a2112fa98b69e2fd38a11b7f35
Rene Rivera <grafikrobot@gmail.com>
2018-04-24 22:20:52 -0500

    Use OpenMPI conan package and related test changes [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,6 +13,9 @@ class TestPackageConan(ConanFile):
         boost_deps = ['assert', 'concept_check', 'config', 'conversion', 'core', 'detail', 'function_types', 'fusion', 'iterator', 'mpl', 'optional', 'smart_ptr', 'static_assert', 'type_traits', 'utility']
         for lib in boost_deps:
             self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
+        if False:
+            if not tools.os_info.is_windows:
+                self.requires("openmpi/3.0.0@bincrafters/stable")
 
     def build(self):
         cmake = CMake(self)
