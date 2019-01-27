0f2ebb38795394d1a69c9c3e72dbd6f937c71a05
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:47 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostIteratorConan(base.BoostBaseConan):
     name = "boost_iterator"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_iterator"
     lib_short_names = ["iterator"]
     header_only_libs = ["iterator"]
