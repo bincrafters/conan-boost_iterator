62b24ba66cc9e64cc7dd153cc6e2008e47217e6d
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,27 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostIteratorConan(base.BoostBaseConan):
     name = "boost_iterator"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_iterator"
-    lib_short_names = ["iterator"]
-    header_only_libs = ["iterator"]
-    b2_requires = [
-        "boost_assert",
-        "boost_concept_check",
-        "boost_config",
-        "boost_conversion",
-        "boost_core",
-        "boost_detail",
-        "boost_function_types",
-        "boost_fusion",
-        "boost_mpl",
-        "boost_optional",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
